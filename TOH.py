ee02b3e83fd9bcc1e590c5999332c2e827adfe38
CHALLA ESWAR VENKATA DURGA RAO <98168795+eswarchalla9642@users.noreply.github.com>
2022-08-21 14:17:51 +0530

    Towers_of_hanoi_path_approach

--- TOH.py
+++ TOH.py
@@ -1,8 +1,20 @@
-def toh(n,fro,to,aux):
-    if n==1:
-        print("move disk 1 from rod ",fro," to rod ",to)
-        return
-    toh(n-1,fro,aux,to)
-    print("move disk",n,"from rod ",fro," to rod ",to)
-    toh(n-1,aux,to,fro)
-toh(3,'a','b','c')
+A = [3, 2, 1]
+B = []
+C = []
+
+def move(n, source, target, auxiliary):
+    if n > 0:
+        # Move n - 1 disks from source to auxiliary, so they are out of the way
+        move(n - 1, source, auxiliary, target)
+
+        # Move the nth disk from source to target
+        target.append(source.pop())
+
+        # Display our progress
+        print(A, B, C, "->",end="")
+
+        # Move the n - 1 disks that we left on auxiliary onto target
+        move(n - 1, auxiliary, target, source)
+
+# Initiate call from source A to target C with auxiliary B
+move(3, A, C, B)
